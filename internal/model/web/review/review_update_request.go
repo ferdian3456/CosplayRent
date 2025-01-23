239bd10a88410319b413d7542f043cf407096ae4
ferdian3456 <mulyadipermadi21@gmail.com>
2025-01-23 11:38:12 +0700

    chore: delete validation in ReviewUpdateRequest struct

--- internal/model/web/review/review_update_request.go
+++ internal/model/web/review/review_update_request.go
@@ -2,6 +2,6 @@ package review
 
 type ReviewUpdateRequest struct {
 	Review_picture *string `json:"review_picture,omitempty"`
-	Description    string  `validate:"min=5,max=1000" json:"description,omitempty"`
+	Description    string  `json:"description,omitempty"`
 	Rating         int     `json:"rating,omitempty" `
 }
